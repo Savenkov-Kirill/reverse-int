8fcf304411e9cb2f17bdbedea89c2826225d5aac
Kirill Savenkov <ksavenkov@mail.ru>
2024-09-10 13:08:30 +0300

    init: start task Reverse Int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    const reversedNumber = parseInt(Math.abs(n).toString().split('').reverse().join(''), 10);
+    return reversedNumber;
+};
